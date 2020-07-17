b203874245e744b6e7e235a50915c5eb3f26c712
sevda <imanysevda@gmail.com>
2020-07-17 20:13:34 +0430

    add articles urls to main urls.py

--- djangoBlog/djangoBlog/urls.py
+++ djangoBlog/djangoBlog/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , inlcude
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
-    path('', views.home)
+    path('', views.home),
+    path('articles/', include('articles.urls'))
 
 ]
