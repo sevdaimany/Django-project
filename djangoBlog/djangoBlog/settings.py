e37ef9b3c39f4bc31c2089bdefbd801388b8e393
sevda <imanysevda@gmail.com>
2020-07-17 19:25:39 +0430

    add templates directory

--- djangoBlog/djangoBlog/settings.py
+++ djangoBlog/djangoBlog/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'djangoBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
