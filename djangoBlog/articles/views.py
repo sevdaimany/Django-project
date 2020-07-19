6096b6a7cc47fc4009fcd2469bbdef02101df66f
sevda <imanysevda@gmail.com>
2020-07-19 20:37:03 +0430

    get data from  database and pass as render parameter

--- djangoBlog/articles/views.py
+++ djangoBlog/articles/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 
 def articles_list(request):
-    return render(request , 'articles/articleslist.html')
+    articles  = models.Article.objects.all().order_by('date')
+    args = {'articles':articles}
+    return render(request , 'articles/articleslist.html',args)
 
