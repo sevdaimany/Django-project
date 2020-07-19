94cf9e1dd400d547184b7a91faee4f20baf0e128
sevda <imanysevda@gmail.com>
2020-07-19 19:16:04 +0430

    add __str__

--- djangoBlog/articles/models.py
+++ djangoBlog/articles/models.py
@@ -6,3 +6,7 @@ class Article(models.Model):
     slug =models.SlugField()
     body =models.TextField()
     date =models.DateTimeField(auto_now_add = True)
+
+
+    def __str__(self):
+        return self.title
