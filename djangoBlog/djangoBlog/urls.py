88a52bfa2530c7dc346c73598b8b862fa66db522
sevda <imanysevda@gmail.com>
2020-07-20 09:02:27 +0430

    add static file

--- djangoBlog/djangoBlog/urls.py
+++ djangoBlog/djangoBlog/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path , include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +10,5 @@ urlpatterns = [
     path('articles/', include('articles.urls'))
 
 ]
+
+urlpatterns += staticfiles_urlpatterns()
