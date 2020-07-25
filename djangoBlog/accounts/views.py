8378c2bb65de89d04009c03ac60b26c8b9666e2f
sevda <imanysevda@gmail.com>
2020-07-25 09:50:48 +0430

    add signup_view function to views.py

--- djangoBlog/accounts/views.py
+++ djangoBlog/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def signup_view(request):
+    form = UserCreationForm()
+    return render(request,'accounts/signup.html',{'form':form })
