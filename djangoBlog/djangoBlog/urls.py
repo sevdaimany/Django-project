ccfc0eb07c1d3a6fadcc7c312c3c4d647bed6399
sevda <imanysevda@gmail.com>
2020-08-02 11:56:41 +0430

    styling

--- djangoBlog/djangoBlog/urls.py
+++ djangoBlog/djangoBlog/urls.py
@@ -4,14 +4,14 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
+from articles.views import articles_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
-    path('', views.home),
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
-
+    path('',articles_list),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
