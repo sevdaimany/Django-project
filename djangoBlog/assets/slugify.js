1e76bfae91f12bfa1d30c16d694c99c032ac1464
sevda <imanysevda@gmail.com>
2020-08-02 12:39:58 +0430

    slug

---
+++ djangoBlog/assets/slugify.js
@@ -0,0 +1,13 @@
+const  titleInput = document.querySelector('input[name=title]')
+const  slugInput = document.querySelector('input[name=slug]')
+
+
+const slugify = (val) =>{
+    return val.toString().trim()
+      .replace(/&/g,'-and-')
+      .replace(/[\s\W-]+/g,'-')
+};
+
+titleInput.addEventListener('keyup',(e)=>{
+    slugInput.setAttribute('value',slugify(titleInput.value)); 
+});
